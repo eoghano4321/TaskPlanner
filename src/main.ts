990fbfb7165b003f2c296a05b4890057c9871093
eoghano4321 <eoghano4321@gmail.com>
2021-12-29 17:50:15 +0000

    Created a debug command that sends a notificiation just to test functionality

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { FileView, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 import type {Moment, WeekSpec} from 'moment';
 import { createDailyNote, getDailyNoteSettings} from 'obsidian-daily-notes-interface';
-  
+
 
 import {SETTINGS} from "./Settings";
 // These are the default values for the settings
@@ -32,19 +32,28 @@ export default class MyPlugin extends Plugin {
 	async onload(){
 		await this.loadSettings();
 
-
+		// TODO Create a script to handle note creation
 		this.addCommand({
 			id: 'open_task_planner',
 			name: 'Open Task Planner Note',
 			callback: () => {
-				createDailyNote(date)
-			}
-			
+				//(file) => {
+				// activeFile.setFile(file).createDailyNote(date)
+				this.send_notif(true);
+				//}.
+			}	
 		});
 
 	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, this.settings.OpenOnStart, await this.loadData());
+	}
+
+	private send_notif(test?: boolean) {
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (markdownView) {
+			new Notification(`Test Notif`, { body: `This is a test notification ${test}`, requireInteraction: true });
+		}
 	}
 }
