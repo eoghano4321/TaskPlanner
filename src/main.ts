9ec60546f866fa49ebe7f223c95436d46779dd2f
eoghano4321 <eoghano4321@gmail.com>
2021-12-29 13:29:44 +0000

    Added setting defaults

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-import {TASK_SETTINGS} from "./Settings";
+import {SETTINGS} from "./Settings";
 
 export const NAME: string = "Eoghan";
 
-console.log(TASK_SETTINGS);
+
+const PluginSettings: SETTINGS = {
+	OpenOnStart : true,
+	SideButton : true,
+	SendNotifs : true,
+	StatusBar : true
+}
 
 export default class MyPlugin extends Plugin {
-	settings: typeof TASK_SETTINGS;
+	settings = PluginSettings;
+
+	async onload(){
+		await this.loadSettings();
+
+
+	}
+
+
+	async loadSettings() {
+		this.settings = Object.assign({}, PluginSettings.OpenOnStart, await this.loadData());
+	}
 }
