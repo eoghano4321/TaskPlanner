09d6eb099562e4381e70235b2e758d262b742dfb
eoghano4321 <eoghano4321@gmail.com>
2021-12-30 18:09:13 +0000

    Added command to create a new note

---
+++ src/CreateTaskNote.ts
@@ -0,0 +1,1 @@
+import {DefaultPluginSettings } from './main'
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { FileView, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, ToggleComponent } from 'obsidian';
+import { FileView, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, ToggleComponent, Vault, normalizePath } from 'obsidian';
 import type {Moment, WeekSpec} from 'moment';
 import { createDailyNote, getDailyNoteSettings} from 'obsidian-daily-notes-interface';
 
@@ -8,7 +8,7 @@ import {SETTINGS} from "./Settings";
 // TODO - Add the ability to change those settings in the settings tab & have those values saved
 // TODO - Have the settings variable check the loaded settings after await this.loadSettings(); and replace the defaults with those values
 
-const DefaultPluginSettings: SETTINGS = {
+export const DefaultPluginSettings: SETTINGS = {
 	OpenOnStart : false,
 	SideButton : false,
 	SendNotifs : false,
@@ -27,10 +27,17 @@ declare global {
   }
 
 export default class MyTaskPlugin extends Plugin {
+	vault : Vault = this.app.vault;
 	settings = DefaultPluginSettings;
+
+
+
 
 	async onload(){
 		await this.loadSettings();
+
+		// const normalizedPath = normalizePath(`'Task'`);
+		// const FileExists = await this.vault.adapter.exists(normalizedPath, false);
 
 		this.addSettingTab(new SettingTab(this.app, this));
 
@@ -41,9 +48,27 @@ export default class MyTaskPlugin extends Plugin {
 			callback: () => {
 				//(file) => {
 				// activeFile.setFile(file).createDailyNote(date)
-				this.send_notif(true);
+				this.send_notif();
 				//}.
 			}	
+		});
+
+		this.addCommand({
+			id: 'create_task_note',
+			name: 'Create a Task Planner Note',
+			callback: () => {
+				this.createFileIfNotExists('TAskPlanner.md');
+				
+				
+				// if (!FileExists){
+				// 	this.vault.create(normalizedPath, `## TASKKKKKS`);
+				// 	this.send_notif('File Created ');
+
+				// }
+
+
+			}
+
 		});
 
 	}
@@ -52,12 +77,30 @@ export default class MyTaskPlugin extends Plugin {
 		this.settings = Object.assign({}, this.settings.OpenOnStart, await this.loadData());
 	}
 
-	public send_notif(test?: boolean) {
+	public send_notif(message: string = `This is a test notification`, test?: boolean) {
 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (markdownView) {
-			new Notification(`Test Notif`, { body: `This is a test notification ${test}`, requireInteraction: true });
+			new Notification(`Test Notif`, { body: message + `${test}`, requireInteraction: true });
+			
 		}
 	}
+
+	async createFileIfNotExists(fileName: string) {
+		this.send_notif('DEBUG1')
+        try {
+            const normalizedFileName = normalizePath(fileName);
+            if (!await this.vault.adapter.exists(normalizedFileName, false)) {
+                await this.vault.create(normalizedFileName, `## TAslks
+				- []`);
+				this.send_notif(normalizedFileName)
+            }
+			else{
+				this.send_notif(`File ${normalizedFileName} already exists`, true)
+			}
+        } catch (error) {
+            this.send_notif(`Error ${error}`)
+        }
+    }
 }
 
 export class SettingTab extends PluginSettingTab {
@@ -77,16 +120,29 @@ export class SettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('Open on Start')
-			.setDesc('Open the days task planner on startup')
+			.setDesc('Open the Task Note on startup')
 
 			.addToggle(toggle => 
 				toggle
 					.setValue(this.plugin.settings.OpenOnStart)
 					.onChange( async (value: boolean) => {	
 						this.plugin.settings.OpenOnStart = value;
-						await this.plugin.send_notif(this.plugin.settings.OpenOnStart);
+						this.plugin.send_notif(`This is the on start setting `,this.plugin.settings.OpenOnStart);
 						//await this.plugin.loadSettings();
 				}));
+			
+		new Setting(containerEl)
+			.setName('Side Bar Button')
+			.setDesc('Add a side bar button to open the Task Note')
+
+			.addToggle(toggle => 
+				toggle
+					.setValue(this.plugin.settings.SideButton)
+					.onChange( async (value: boolean) => {	
+						this.plugin.settings.SideButton = value;
+						this.plugin.send_notif(`This is the sidebutton setting `, this.plugin.settings.SideButton);
+						//await this.plugin.loadSettings();
+				}));	
 			
 
 			// .addText(text => text
